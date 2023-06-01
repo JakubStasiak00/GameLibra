23aeac7ee847f4eaa73f0ab8cfa193bb923edffb
JakubStasiak00 <125593509+JakubStasiak00@users.noreply.github.com>
2023-06-01 18:17:18 +0200

    added search bar and select tag fields to the library

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="q-pa-md">
+
+    <q-row class="ritems-center q-px-lg q-mb-md" :class="$q.screen.lt.sm ? 'col' : 'row' ">
+      <div class="q-col-xs col q-pa-sm">
+        <q-input rounded outlined class="a-px-lg" label="browse library" v-model="usersGame"></q-input>
+      </div>
+      <div class="q-col-xs col q-pa-sm">
+        <q-select :options="getTags(games)" rounded outlined class="a-px-lg " label="search by tags" v-model="selectedTag" />
+      </div>
+    </q-row>
+
     <div class="row q-gutter-md q-col-lg-3 justify-center">
       <q-card class="card xs-col-12 sm-col-6 md-col-4 lg-col-3 flex custom-center" style="width: 250px;">
         <q-card-section>
@@ -7,7 +17,7 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="card xs-col-12 sm-col-6 md-col-4 lg-col-3" v-for="game in games" :key="game" style="width:250px;">
+      <q-card class="card xs-col-12 sm-col-6 md-col-4 lg-col-3" v-for="game in formatedGames" :key="game" style="width:250px;">
         <q-img :src="game.background_image" cover class="absolute-full" />
         <q-btn @click="deleteGame(game)" flat rounded size="md" class="q-px-sm absolute text-white" icon="close" style="z-index: 10;" />
         <q-card-section>
@@ -40,7 +50,7 @@
       </q-card-section>
 
       <q-card-section>
-        <q-form @submit.prevent="searchGame" class="row">
+        <q-form @submit.prevent="searchGame" class="row no-wrap">
           <q-input standout v-model="gameToFind" label="Find your game">
             <template v-slot:prepend>
               <q-icon name="search" />
@@ -77,15 +87,59 @@ import { db } from 'src/firebase/main';
 import { collection, deleteDoc, doc, getDoc, onSnapshot, setDoc } from '@firebase/firestore';
 import { useUserStore } from 'src/stores/userStore';
 import { watchEffect } from 'vue';
+import { computed } from 'vue';
+import { useQuasar } from 'quasar';
 
 const userStore = useUserStore()
+const $q = useQuasar()
 
 const addGames = ref(false)
 const gameToFind = ref('')
 const foundGame = ref(null)
 const error = ref('')
 
+const selectedTag = ref('')
+const usersGame = ref('')
+
 const games = ref([])
+
+
+const getTags = (games) => {
+  let gameTags = new Set
+
+  games.forEach(game => {
+    game.tags.forEach(tag => {
+      gameTags.add(tag.slug)
+    })
+
+  })
+
+  return new Array('', ...gameTags)
+}
+
+const formatedGames = computed(() => {
+  let formatGames = new Array(...games.value)
+
+  if(selectedTag.value !== '') {
+    formatGames = formatGames.filter(game => {
+      let wasFound = false
+
+      game.tags.forEach(tag => {
+        if(tag.slug === selectedTag.value) {
+          wasFound = true
+        }
+      })
+
+      return wasFound
+    })
+  }
+
+  if(usersGame.value !== '') {
+    formatGames = formatGames.filter(game => game.name.toLowerCase().includes(usersGame.value.toLowerCase()))
+  }
+
+  return formatGames
+})
 
 const handleAddingGame = () => {
   addGames.value = true
@@ -158,12 +212,14 @@ const deleteGame = async game => {
 
 watchEffect(async () => {
   if (userStore.isLogged) {
-    const unsubscribe = onSnapshot(await gettingLibraryReference(), snapshot => {
+    const unsubscribe = onSnapshot(gettingLibraryReference(), snapshot => {
       games.value = []
 
       snapshot.forEach(doc => {
         games.value.push(doc.data())
       })
+
+      console.log(games.value[0].tags[0].slug)
     })
   }
 })
